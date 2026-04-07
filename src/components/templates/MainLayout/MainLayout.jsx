f9013901ec0cc1226b628f4793398377bc0974a0
ZhytomyrskaAnna <zhytomyreti@gmail.com>
2026-04-07 12:53:16 +0300

    Update MainLayout

--- src/components/templates/MainLayout/MainLayout.jsx
+++ src/components/templates/MainLayout/MainLayout.jsx
@@ -3,6 +3,8 @@ import { useContext } from 'react';
 import { AuthContext } from '../../../context/AuthContext';
 import styles from './MainLayout.module.css';
 import Header from '../../organisma/Header';
+import Button from '../../atoms/Button/Button';
+import useThemeContext from '../../../hooks/useThemeContext';
 
 const MainLayout = () => {
 
@@ -11,8 +13,20 @@ const MainLayout = () => {
   const getActiveClass = ({ isActive }) =>
     isActive ? `${styles.link} ${styles.active}` : styles.link;
 
+  const [theme, setTheme] = useThemeContext;
+
+  const toggleTheme = () => {
+    setTheme(theme === 'light' ? 'daek' : 'light');
+  };
+
+  const themeIcon = theme === 'light' ? '☀️' : '🌙';
+
+  const themeButtonText = `${themeIcon} ${theme === 'light' ? 'Світла тема' : 'Темна тема'}`;
+
+  const themeButton = <Button onClick={toggleTheme}>{themeButtonText}</Button>
+
   return (
-    <div className={styles.wrapper}>
+    <div className={styles.wrapper} data-theme={theme}>
         <Header />
       <nav className={styles.navbar}>
         <NavLink to="/" className={getActiveClass} end>Головна</NavLink>
@@ -24,6 +38,7 @@ const MainLayout = () => {
         <NavLink to="/practice5" className={getActiveClass}>Практична робота №5</NavLink>
         <NavLink to="/practice6" className={getActiveClass}>Практична робота №6</NavLink>
         <NavLink to="/practice7" className={getActiveClass}>Практична робота №7</NavLink>
+        <NavLink to="/practice8" className={getActiveClass}>Практична робота №8</NavLink>
         <NavLink to="/lab1" className={getActiveClass}>Лабораторна робота №1</NavLink>
         <NavLink to="/lab2" className={getActiveClass}>Лабораторна робота №2</NavLink>
         <NavLink to="/lab3" className={getActiveClass}>Лабораторна робота №3</NavLink>
@@ -38,6 +53,7 @@ const MainLayout = () => {
             <NavLink to="/register" className={getActiveClass}>Реєстрація</NavLink>
           </>
         )}
+        {themeButton}
       </nav>
       <main className={styles.mainContent}>
         <Outlet />
